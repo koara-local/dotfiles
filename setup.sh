301e6426c0ad4b64961e6c6fc30f43147ee1e6ea
Kohei Arao <koara.local.jp@gmail.com>
2014-03-07 18:38:33 +0900

    Add NoeBundle & .gitconfig.local Install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Link
 DOT_FILES=( \
 	.gitconfig \
 	.bashrc \
@@ -10,3 +11,22 @@ for file in ${DOT_FILES[@]}
 do
 ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+# Copy
+if [ ! -e ~/.gitconfig.local ]; then
+    cp $HOME/dotfiles/.gitconfig.local $HOME/.gitconfig.local
+fi
+
+# [Vim] NeoBundle
+if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+
+    [ ! -d ~/.vim/bundle] && mkdir -p ~/.vim/bundle
+
+    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+    if [ $? -ne 0 ]; then
+        echo "------------------------------------------------------------------ "
+        echo " Install NeoBundle failed."
+        echo " Please check .gitconfig.local(proxy setting) or your network settings"
+        echo "------------------------------------------------------------------ "
+    fi
+fi
