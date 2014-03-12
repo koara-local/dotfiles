7458bdfda8afdf59599bdc416c3c4cae9a2bca1b
Kohei Arao <koara.local.jp@gmail.com>
2014-03-12 13:01:45 +0900

    setup.sh: Install NeoBundle at first

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# [Vim] NeoBundle
+if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+    # Check ~/.vim/bundle
+    [ ! -d ~/.vim/bundle] && mkdir -p ~/.vim/bundle
+    # Install NeoBundle
+    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+    if [ $? -ne 0 ]; then
+        echo "------------------------------------------------------------------ "
+        echo " Install NeoBundle failed."
+        echo " Please check .gitconfig.local(proxy setting) or your network settings"
+        echo "------------------------------------------------------------------ "
+        # Exit shell
+        exit 0
+    fi
+fi
+
 # Link
 DOT_FILES=( \
+    .vimrc \
     .gitconfig \
     .bashrc \
-    .vimrc \
     .gdbinit \
     .tmux.conf \
 )
@@ -17,18 +33,4 @@ done
 # Copy
 if [ ! -e ~/.gitconfig.local ]; then
     cp $HOME/dotfiles/.gitconfig.local $HOME/.gitconfig.local
-fi
-
-# [Vim] NeoBundle
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-
-    [ ! -d ~/.vim/bundle] && mkdir -p ~/.vim/bundle
-
-    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-    if [ $? -ne 0 ]; then
-        echo "------------------------------------------------------------------ "
-        echo " Install NeoBundle failed."
-        echo " Please check .gitconfig.local(proxy setting) or your network settings"
-        echo "------------------------------------------------------------------ "
-    fi
 fi
