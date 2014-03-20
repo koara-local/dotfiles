d6ba65a2a1840228a8d7b294c58ec89400646f66
Kohei Arao <koara.local.jp@gmail.com>
2014-03-20 13:55:03 +0900

    setup.sh: Check an existing .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
+# エラー用 echo [赤・太字]
 function echo_error_ { echo -e "\e[1;31m$*\e[m"; }
 
-# [Vim] NeoBundle
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-    # Check ~/.vim/bundle
-    [ ! -d ~/.vim/bundle] && mkdir -p ~/.vim/bundle
-    # Install NeoBundle
-    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-    if [ $? -ne 0 ]; then
-        echo_error_ " ----------------------------------------------------------------------- "
-        echo_error_ "  Install NeoBundle failed."
-        echo_error_ "  Please check .gitconfig.local(proxy setting) or your network settings"
-        echo_error_ " ----------------------------------------------------------------------- "
-        # Exit shell
-        exit 0
+# Copy
+if [ ! -e ~/.gitconfig.local ]; then
+    # ローカル設定を退避、なければテンプレート配置
+    if [ -e ~/.gitconfig ]; then
+        cp $HOME/.gitconfig $HOME/.gitconfig.local
+    else
+        cp $HOME/dotfiles/.gitconfig.local $HOME/.gitconfig.local
     fi
 fi
 
@@ -34,7 +29,18 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-# Copy
-if [ ! -e ~/.gitconfig.local ]; then
-    cp $HOME/dotfiles/.gitconfig.local $HOME/.gitconfig.local
+# [Vim] NeoBundle の設定
+if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+    # Check ~/.vim/bundle
+    [ ! -d ~/.vim/bundle] && mkdir -p ~/.vim/bundle
+    # Install NeoBundle
+    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+    if [ $? -ne 0 ]; then
+        echo_error_ " ----------------------------------------------------------------------- "
+        echo_error_ "  Install NeoBundle failed."
+        echo_error_ "  Please check .gitconfig.local(proxy setting) or your network settings"
+        echo_error_ " ----------------------------------------------------------------------- "
+        # Exit shell
+        exit 0
+    fi
 fi
