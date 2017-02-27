17704bb4040fce103ee4e9141ec1918fb82c238a
Kohei Arao <koara.local.jp@gmail.com>
2017-02-27 16:27:36 +0900

    update git build.sh : install package automatically

--- build/git/build.sh
+++ build/git/build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -xeu
 
+declare -a info=($(../../bin/get_os_info))
+
+case ${info[0]} in
+ubuntu)
+    echo "ubuntu"
+
+    sudo apt-get install \
+        libcurl4-openssl-dev \
+        libexpat1-dev 
+
+    ;;
+*)
+    echo "unsupported"
+    exit 1
+    ;;
+esac
+
 if [ ! -d git-master ]; then
   wget https://github.com/git/git/archive/master.zip
   unzip master.zip
